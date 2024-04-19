d991e21b7ea610feaa9916ae2a2ea644fe306071
Yosthim <a20201497@pucp.edu.pe>
2024-04-18 21:04:57 -0500

    Creación del proyecto en Intellij

---
+++ src/main/java/com/example/lab3_20201497/Lab320201497Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_20201497;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320201497Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab320201497Application.class, args);
+	}
+
+}
